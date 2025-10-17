051d2a1f37aacd23fa2c72a3397fd91d32996c33
Einar <einarsmari@gmail.com>
2025-10-17 21:06:09 +0000

    add Contact info

--- my-portfolio/app/Sections/Contact.tsx
+++ my-portfolio/app/Sections/Contact.tsx
@@ -1,4 +1,18 @@
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div>
+        <div>Email:</div>
+        <div>einarsmari@gmail.com</div>
+      </div>
+      <div>
+        <div>LinkedIn:</div>
+        <div>
+          https://www.linkedin.com/in/einar-sm%C3%A1ri-einarsson-5098b46a/
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 export default Contact;
